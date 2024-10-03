f05df8fa4d182b6b1a82cf970537873b18e17f4c
David4870 <dandavid730@gmail.com>
2024-10-03 21:09:39 +0200

    v0.1.0 Outsource logic from main function
    Outsource app logic from the main function into multiple files.

---
+++ src/game.cpp
@@ -0,0 +1,79 @@
+#include <iostream>
+
+#include <SDL3/SDL.h>
+#include <GL/glew.h>
+
+#include "appContext.hpp"
+#include "game.hpp"
+
+Game::Game()
+{
+    initialize();
+}
+
+Game::~Game()
+{
+    quit();
+}
+
+void Game::initialize()
+{
+    if (SDL_Init(SDL_INIT_VIDEO) != 0)
+    {
+        std::cout << "Error: SDL_Init(): " << SDL_GetError() << std::endl; 
+    }
+
+    context::window = SDL_CreateWindow("Graphics-Demo", context::WINDOW_WIDTH, context::WINDOW_HEIGHT, SDL_WINDOW_OPENGL);
+    if (context::window == nullptr) {
+        std::cout << "Error: SDL_CreateWindow(): " << SDL_GetError() << std::endl; 
+    }
+
+    SDL_GLContext glContext = SDL_GL_CreateContext(context::window);
+    if (glContext == nullptr)
+    {
+        std::cout << "Error: SDL_GL_CreateContext():" << SDL_GetError() << std::endl; 
+    }
+
+    GLenum err = glewInit();
+    if (err != GLEW_OK)
+    {
+        std::cout << "Error: glewGetErrorString(): " << SDL_GetError() << std::endl; 
+    }
+}
+
+void Game::processEvents()
+{
+    using namespace context;
+    
+    while (SDL_PollEvent(&event))
+    {
+        if (event.type == SDL_EVENT_QUIT || (event.type == SDL_EVENT_KEY_DOWN && event.key.key == SDLK_ESCAPE))
+            running = false;
+    }
+}
+
+void Game::run()
+{
+    while (context::running)
+    {
+        processEvents();
+
+        glClearColor(50 / 255.0f, 50 / 255.0f, 50 / 255.0f, 255 / 255.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glBegin(GL_TRIANGLES);
+        glVertex2f(-0.5f, -0.5f);
+        glVertex2f(0.0f, 0.5f);
+        glVertex2f(0.5f, -0.5f);
+        glEnd();
+
+        SDL_GL_SwapWindow(context::window);
+    }
+}
+
+void Game::quit()
+{
+    SDL_GL_DestroyContext(glContext);
+    SDL_DestroyWindow(context::window);
+    SDL_Quit();
+}
--- src/main.cpp
+++ src/main.cpp
@@ -3,66 +3,15 @@
 #include <SDL3/SDL.h>
 #include <GL/glew.h>
 
-void getSDLVersion();
+#include "game.hpp"
+#include "versionUtils.hpp"
 
 int main(int argc, char* argv[])
 {
     getSDLVersion();
 
-    if (SDL_Init(SDL_INIT_VIDEO) != 0)
-    {
-        std::cout << "Error: SDL_Init(): " << SDL_GetError() << std::endl; 
-    }
+    Game game;
+    game.run();
 
-    SDL_Window *window;
-    window = SDL_CreateWindow("Graphics-Demo", 1920, 1080, SDL_WINDOW_OPENGL);
-    if (window == NULL) {
-        std::cout << "Error: SDL_CreateWindow(): " << SDL_GetError() << std::endl; 
-    }
-
-    SDL_GLContext glContext = SDL_GL_CreateContext(window);
-
-    bool running = true;
-    SDL_Event e;
-
-    while (running)
-    {
-        while (SDL_PollEvent(&e))
-        {
-            if (e.type == SDL_EVENT_QUIT || (e.type == SDL_EVENT_KEY_DOWN && e.key.key == SDLK_ESCAPE))
-                running = false;
-        }
-
-        glClearColor(50 / 255.0f, 50 / 255.0f, 50 / 255.0f, 1.0f);
-        glClear(GL_COLOR_BUFFER_BIT);
-
-        glBegin(GL_TRIANGLES);
-        glVertex2f(-0.5f, -0.5f);
-        glVertex2f(0.0f, 0.5f);
-        glVertex2f(0.5f, -0.5f);
-        glEnd();
-
-        SDL_GL_SwapWindow(window);
-    }
-
-    SDL_GL_DestroyContext(glContext);
-    SDL_DestroyWindow(window);
-    SDL_Quit();
     return 0;
-}
-
-void getSDLVersion()
-{
-    const int compiled = SDL_VERSION;
-    const int linked = SDL_GetVersion();
-
-    SDL_Log("========== Compiled against SDL version %d.%d.%d ==========\n",
-            SDL_VERSIONNUM_MAJOR(compiled),
-            SDL_VERSIONNUM_MINOR(compiled),
-            SDL_VERSIONNUM_MICRO(compiled));
-
-    SDL_Log("========== Linking  against SDL version %d.%d.%d ==========\n\n",
-            SDL_VERSIONNUM_MAJOR(linked),
-            SDL_VERSIONNUM_MINOR(linked),
-            SDL_VERSIONNUM_MICRO(linked));
 }
